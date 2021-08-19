012b92df40570c1db35daa37a8563d75c6ad43c4
HuaxinZhang <dianazhang0219@gmail.com>
2021-08-19 13:38:51 +0800

    basic header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,26 @@
 import './Header.css';
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
 
 function Header() {
+    let Username = "ZhangSan";
+    function signOut(){
+        alert("signout");
+    }
     return (
         <div className="header">
-            <h1>Header</h1>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand>债券销售管理系统</Navbar.Brand>
+
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav.Link eventKey="disabled" disabled>
+                            Signed in as: {Username}
+                        </Nav.Link>
+                        <Button variant="outline-dark" onClick={signOut}>登出</Button>
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
         </div>
     );
 }
