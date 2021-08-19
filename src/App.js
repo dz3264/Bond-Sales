1f49eee088a0422963f2810f5d35dc6fe48cb9fc
HuaxinZhang <dianazhang0219@gmail.com>
2021-08-19 15:11:22 +0800

    insert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./Components/Header";
+import Record from "./Components/Record";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Record/>
     </div>
   );
 }
