2abb8199cff8d2fa5ad2ffe6561d0047851fe7ce
HuaxinZhang <dianazhang0219@gmail.com>
2021-08-21 15:04:15 +0800

    new login form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,40 +1,12 @@
 import './Style.css';
-import {Button, Card, Form} from 'react-bootstrap';
-import { makeStyles } from '@material-ui/core/styles';
+import {Button} from 'react-bootstrap';
 import {useEffect, useState} from "react";
+import LoginModal from "./LoginModal";
 import axios from "axios";
-
 
 export default function Login(){
 
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState(-1);
-
-    function submitSearch() {
-        alert('欢迎'+name+'，'+'正在登陆中，请稍等');
-    }
-
-    useEffect(() => {
-        const fetchName = async () => {
-            let data = {"name":"1234","password":"yyyyyy"};
-            const nameResult = await axios.post('${this.$url}/test/testRequest',data)
-                .then(res=>{
-                    console.log('res=>',res);
-                });
-
-            setName(nameResult.data);
-        };
-        // const fetchPassword = async () => {
-        //     const passwordResult = await axios(
-        //         '/api/ListBond'
-        //     );
-        //
-        //     setPassword(passwordResult.data);
-        // };
-        fetchName();
-        // fetchPassword();
-    }, []);
-
+    const [modalShow, setModalShow] = useState(false);
 
     return (
         <section id="hero" className="d-flex align-items-center">
@@ -43,7 +15,7 @@ export default function Login(){
                 <h1>欢迎使用 <span>债券销售管理系统</span></h1>
                 <h2>Welcome to Bond sales management system</h2>
                 <div className="btn-group-sm">
-                    <Button className={"btn"} variant="primary" onClick={submitSearch} type="submit">
+                    <Button className={"btn"} variant="primary" onClick={() => setModalShow(true)} type="submit">
                         登陆
                     </Button>
 
@@ -53,41 +25,10 @@ export default function Login(){
 
                 </div>
             </div>
+            <LoginModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
         </section>
-        // <div className="login"  align="center">
-        //
-        //     <h1>Login</h1>
-        //     <p>Welcome</p>
-        //
-        // <br></br><br></br><br></br><br></br>
-        //
-        //     <Card style={{ width: '18rem' }}>
-        //         <Card.Body>
-        //
-        //                 <Form.Group className="mb-3" controlId="Name">
-        //                     <Form.Label>账号</Form.Label>
-        //                     <Form.Control
-        //                         type="email"
-        //                         placeholder="请输入你的名字"
-        //                         onChange={(name) => setName(name.target.value)}
-        //                     />
-        //                 </Form.Group>
-        //
-        //                 <Form.Group className="mb-3" controlId="formBasicPassword">
-        //                     <Form.Label>密码</Form.Label>
-        //                     <Form.Control onChange={(password) => setPassword(password.target.value)}
-        //                                   type="password" placeholder="请输入你的密码" />
-        //                 </Form.Group>
-        //                 <Button onClick={submitSearch}variant="outline-dark" type="submit">
-        //                     登陆
-        //                 </Button>
-        //
-        //             {/*onClick={() => { alert('正在登陆中，请稍等') }}*/}
-        //
-        //         </Card.Body>
-        //     </Card>
-        //
-        //
-        // </div>
     );
 }
---
+++ src/Components/LoginModal.js
@@ -0,0 +1,62 @@
+import './Style.css';
+import {Button, Col, Form, Modal} from 'react-bootstrap';
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export default function SignInModal(props) {
+
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState(-1);
+
+    function submitLogin() {
+        alert('欢迎'+name+'，'+'正在登陆中，请稍等');
+    }
+
+    useEffect(() => {
+        const fetchName = async () => {
+            let data = {"name":"1234","password":"yyyyyy"};
+            const nameResult = await axios.post('${this.$url}/test/testRequest',data)
+                .then(res=>{
+                    console.log('res=>',res);
+                });
+
+            setName(nameResult.data);
+        };
+        fetchName();
+    }, []);
+
+    return (
+        <Modal
+            {...props}
+            size="md"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="modal-title">
+                    欢迎登陆
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Group className="mb-3" controlId="Name">
+                    <Form.Label>账号</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="请输入你的名字"
+                        onChange={(name) => setName(name.target.value)}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>密码</Form.Label>
+                    <Form.Control onChange={(password) => setPassword(password.target.value)}
+                                  type="password"
+                                  placeholder="请输入你的密码" />
+                </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={submitLogin}>登陆</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
